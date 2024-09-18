0a1a92ac0f0516f004a443f1d332c3944c990443
Duda <137117671+duds0901@users.noreply.github.com>
2024-09-18 08:55:44 -0300

    "first commit"

---
+++ AULA04_PROJETO_LOOPS/script.js
@@ -0,0 +1,52 @@
+
+/*
+//whire = enquanto
+
+var x = 0;
+document.write("<br>Testando o loop com whire ");
+while(x<10){
+    document.write('<br>O valor de x é: '+x);
+    //incremento de x 
+    //x = x+1 
+    x++;
+}
+
+//for = para
+var valor = 13
+document.write('<br><br>Testando o loop com For')
+for(a=0;a<=valor;a++){
+    document.write("<br>O valor de a é: "+a);
+    console.log(a*2);
+
+}
+*/
+
+//switch = trocar escolha (escolha)
+
+function pedir(){
+    var valor = prompt('Digite uma opção de 1 a 4');
+
+    console.log(valor);
+    console.log(typeof valor);
+    console.log(typeof Number(valor));
+    console.log(valor);
+
+
+    switch(Number (valor)){
+        case 1:
+            alert('Você escolheu suco');
+            break;
+        case 2:
+            alert('Você escolheu água gelada');
+            break;
+        case 3:
+            alert('Você escolheu sorvete');
+            break;
+        case 4:
+            alert('Você escolheu você chamou o garçom');
+            break;
+        default:
+            alert('Escolha de 1 até 4');
+            break;
+    }
+}
